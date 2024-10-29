49c0a79e316419dbd8d60b97a98c185e4db1a9e5
Micael Macedo <micaelm2009@hotmail.com>
2024-10-28 23:28:18 -0300

    feat: add ngFor

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CursoDetalheComponent } from "./curso-detalhe/curso-detalhe.component";
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [CursoDetalheComponent],
+  //É necessário importar o NgFor para exibir o array
+  imports: [CursoDetalheComponent, NgFor],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
 
+  //nome da variável:tipo de variável
+  //Para definir de qualquer tipo utilize o any
+  nomePortal: string
+
+  //Como Definir listas de algum tipo
+  cursos: string[] = ['Java', 'Ext JS', 'Angular']
+
+
+  constructor() {
+    this.nomePortal = "http://loiane.training"
+
+  }
+
+  ngOnInit() {
+
+  }
 }
